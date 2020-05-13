709777e59da3d5910cad324e22a44d1b3ec45447
Ozhan Esemen <ozhan@esemen.com>
2020-05-13 16:05:39 +0100

    Installs Swagger

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,8 +8,60 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
+/**
+ * @OA\Info(
+ * description="",
+ * version="",
+ * title="jobinhood.co.uk API",
+ * )
+ */
+
+/**
+ * @OA\SecurityScheme(
+ * securityScheme="bearerAuth",
+ * type="http",
+ * scheme="bearer",
+ * bearerFormat="sanctum"
+ * ),
+ */
 class RegisterController extends Controller
 {
+
+    /**
+     * @OA\Post(
+     *     path="/api/user/register",
+     *     summary="Adds a new user",
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="email",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="name",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="password",
+     *                     type="string"
+     *                 ),
+     *                 example={"email": "john@doe.com", "name": "John Doe", "password": "Password"}
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="Created"
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Unprocessable Entity"
+     *     )
+     * )
+     */
+
     public function register(Request $request)
     {
         $inputs = $request->json()->all();
@@ -17,7 +69,7 @@ class RegisterController extends Controller
         $validated = Validator::make($inputs,
             [
                 'name' => ['required'],
-                'password' => ['required', 'min:8', 'confirmed'],
+                'password' => ['required', 'min:8'],
                 'email' => ['required', 'email', 'unique:users']
             ])->validate();
 
