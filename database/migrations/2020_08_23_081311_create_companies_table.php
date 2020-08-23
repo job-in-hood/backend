f43193c24dd60cc5634c66f04c41f264143b9cc2
Ozhan Esemen <ozhan@esemen.com>
2020-08-23 12:37:24 +0100

    Creates Company table

---
+++ database/migrations/2020_08_23_081311_create_companies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->longText('description')->nullable();
+            $table->string('logo')->nullable();
+            $table->unsignedBigInteger('industry_id')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('industry_id')->references('id')->on('industries')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
