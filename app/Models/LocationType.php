084ca2d80f378e9bfb0844fc09a8fa1e600c8ec2
Ozhan Esemen <ozhan@esemen.com>
2020-12-06 19:50:16 +0000

    Adds LocationType Model

--- app/Models/LocationType.php
+++ app/Models/LocationType.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class LocationType extends Model
 {
     use HasFactory;
+
+    public function location() {
+        return $this->hasMany(Location::class);
+    }
 }
