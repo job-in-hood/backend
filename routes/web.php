28decf4f81331df6379c1ab4691fc1f8daaddd05
Ozhan Esemen <ozhan@esemen.com>
2021-01-08 00:17:33 +0000

    Adds Azure Blob Storage Support

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
+//Route::get('test', function () {
+//   return Storage::disk('azure')->download('test/example.txt');
+//});
+
 //Route::prefix('email')->group(function() {
 //    Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 //    Route::post('login', 'Auth\LoginController@login');
